dd1f0b647ce65dcfa8e17be1626a484acd1cae38
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-12-02 16:40:49 -0500

    test fix

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -17,21 +17,21 @@ public class MainTest {
     @Test
     public void addTest(){
 
-        assertEquals("e.g 2 + 2 = 4",calc.add(1,1),2);
+        assertEquals("e.g 2 + 2 = 4",2,calc.add(1,1));
     }
 
     @Test
     public void subtractTest(){
-        assertEquals("e.g 2 - 2 = 0",calc.subtract(2,1),1);
+        assertEquals("e.g 2 - 2 = 0",1,calc.subtract(2,1));
     }
 
     @Test
     public void multiplyTest(){
-        assertEquals("e.g 2 * 2 = 4",calc.multiply(2,1),2);
+        assertEquals("e.g 2 * 2 = 4",2,calc.multiply(2,1));
     }
 
     @Test
     public void divideTest(){
-        assertEquals("e.g 2 / 2 = 1",calc.divide(10,2),5);
+        assertEquals("e.g 2 / 2 = 1",5,calc.divide(10,2));
     }
 }
